6fe7d532183b090bb57191ce1f032a1612576927
Toni <toniprada@gmail.com>
2016-11-02 10:03:26 +0000

    Add proxies support using another repo because of a bug in pip package that I fixed with a PR

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
+import scrapy_proxies
 
 AIRBNB_CLIENT_ID = os.getenv('AIRBNB_CLIENT_ID', '')
-
+#LOG_LEVEL='INFO'
 # Scrapy settings for wallapop project
 #
 # For simplicity, this file contains only settings considered important or
@@ -29,9 +30,9 @@ USER_AGENT = 'okhttp/2.3.0"'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY=0
+DOWNLOAD_DELAY=0.05
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN=16
+#CONCURRENT_REQUESTS_PER_DOMAIN=4
 #CONCURRENT_REQUESTS_PER_IP=16
 
 # Disable cookies (enabled by default)
@@ -73,13 +74,14 @@ ITEM_PIPELINES = {
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 # NOTE: AutoThrottle will honour the standard settings for concurrency and delay
-AUTOTHROTTLE_ENABLED=True
+AUTOTHROTTLE_ENABLED=False
 # The initial download delay
-AUTOTHROTTLE_START_DELAY=3
+#AUTOTHROTTLE_START_DELAY=3
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY=60
+#AUTOTHROTTLE_MAX_DELAY=60
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG=True
+#AUTOTHROTTLE_DEBUG=True
+#CONCURRENT_REQUESTS_PER_DOMAIN=48
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -88,3 +90,22 @@ AUTOTHROTTLE_DEBUG=True
 #HTTPCACHE_DIR='httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES=[]
 #HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+# Retry many times since proxies often fail
+RETRY_TIMES = 10
+# Retry on most error codes since proxies fail for different reasons
+RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+
+# Proxy list containing entries like
+# http://host1:port
+# http://username:password@host2:port
+# http://host3:port
+# ...
+PROXY_LIST = 'input/proxies.txt'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+    'scrapy_proxies.RandomProxy': 100,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+}
